bf31706575cc51099ca03e02c4861a902521a8ea
ronkili <ronaa0269@gmail.com>
2026-09-06 16:19:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
   clientId: "1544017873012269208",
-  guildId: "1298318371087192074",
+  guildId: "1496161732631072878",
 
   prefix: "!",
 
-  staffRoleId: "1535994900309483560",
-  memberRoleId: "1535988944590868581",
+  staffRoleId: "1502608350364565695",
+  memberRoleId: "1502608358463766559",
 
-  ticketCategoryId: "1522510526653927484",
-  ticketStaffRoleId: "1535994900309483560",
-  ticketLogsChannelId: "1544022744113619054"
+  ticketCategoryId: "1503808405662466140",
+  ticketStaffRoleId: "1545862939976798248",
+  ticketLogsChannelId: "1503808452978413709"
 };
